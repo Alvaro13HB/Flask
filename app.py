9b162c9dbbeb491d399fc52a9585b64223033eee
alvaro13hb <alvaro13hb@gmail.com>
2024-08-16 08:31:59 -0300

    16-08

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'e26fad767fe56ff62e15baf85254dfe7ec73fdaa17a827e12f227746e7cd81f5'
 
 @app.route('/')
 def index():
@@ -14,6 +15,17 @@ def index():
 def aula(nome_REAL = 'Pedro Cougo Mesquita', nick = "lalaufrog", gp = 0):
     dados = {"nome": nome_REAL, "nick": nick, "gp": gp}
     return render_template("perdeste.html", dados_player=dados)
+
+
+@app.route('/form')
+def form():
+    return render_template('formulario.html')
+
+
+@app.route('/dados', methods=['POST'])
+def dados():
+    dados = request.form
+    return render_template('dados.html', dados=dados)
 
 
 if __name__ == '__main__':
