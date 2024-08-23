f4d24d934dc4f636efb3b6bf7acc1daac84cd87b
alvaro13hb <alvaro13hb@gmail.com>
2024-08-23 08:35:54 -0300

    23-08

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
 from flask import Flask, render_template, request, flash
-
 app = Flask(__name__)
+from database import db
+from flask_migrate import Migrate
+from models import Produto
 app.config['SECRET_KEY'] = 'e26fad767fe56ff62e15baf85254dfe7ec73fdaa17a827e12f227746e7cd81f5'
+
+conexao = "mysql+pymysql://alunos:cefetmg@127.0.0.1/bd_produtos"
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
 
-@app.route('/perdeste')
-@app.route('/perdeste/<nome_REAL>')
-@app.route('/perdeste/<nome_REAL>/<nick>')
-@app.route('/perdeste/<nome_REAL>/<nick>/<int:gp>')
-def aula(nome_REAL = 'Pedro Cougo Mesquita', nick = "lalaufrog", gp = 0):
-    dados = {"nome": nome_REAL, "nick": nick, "gp": gp}
-    return render_template("perdeste.html", dados_player=dados)
+@app.route('/produto')
+def produto():
+    p = Produto.query.all()
+    return render_template("lista_produtos.html", dados=p)
+
+
+@app.route('/produto/add')
+def produto_add():
+    pass
 
 
 @app.route('/form')
---
+++ models.py
@@ -0,0 +1,18 @@
+from database import db
+
+class Produto(db.Model):
+    __tablename__ = "tb_produto"
+    id_produto = db.Column(db.Integer, primary_key = True)
+    nome = db.Column(db.String(100))
+    preco = db.Column(db.Float(10, 2))
+    quantidade_estoque = db.Column(db.Integer)
+
+
+    def __init__(self, nome, preco, quantidade_estoque):
+        self.nome = nome
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+
+    
+    def __repr__(self):
+        return "<Produto {}>".format(self.nome)
